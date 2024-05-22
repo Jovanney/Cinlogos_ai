0059d187a645b5ea478d8862fe949895f0a162d7
Jovanney <100790418+Jovanney@users.noreply.github.com>
2024-05-22 18:41:17 -0300

    feat: Update landing page with CinLogos AI branding and background beams

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
-import Image from "next/image";
+import { BackgroundBeams } from "@/components/ui/background-beams";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <div className="h-screen w-full rounded-md bg-neutral-500 dark:bg-secondary relative flex flex-col items-center justify-center antialiased">
+      <div className="max-w-2xl mx-auto p-4">
+        <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b  from-white to-black dark:from-neutral-200 dark:to-neutral-600  text-center font-sans font-bold">
+          Wellcome to the CinLogos AI
+        </h1>
+        <p className="dark:text-neutral-500 text-black max-w-lg mx-auto my-2 text-sm text-center relative z-10">
+          CinLogos AI is a platform that allows you to create logos for your
+          company or brand in a simple and fast way. Just enter the name of your
+          company or brand and we will generate a logo for you.
+        </p>
+      </div>
+      <BackgroundBeams />
+    </div>
+  );
 }
--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -9,11 +9,9 @@ import NavBarIconWhite from "@/public/logo-white.png";
 import { useTheme } from "next-themes";
 
 export function NavbarDemo() {
-  const { theme } = useTheme();
-
   return (
-    <div className="relative w-full z-50 flex items-center justify-between px-10 py-2 min-h-fit dark:bg-black/30  bg-white/30 border-b">
-      <p>LOGO AQUI</p>
+    <div className="absolute top-0 left-0 right-0 w-full z-50 flex items-center justify-between px-10 h-[100px] dark:bg-black/30  bg-white/30 border-b">
+      <p className="text-2xl font-semibold text-white ">Cin Logos</p>
       <Navbar />
       <ModeToggle />
     </div>
@@ -36,34 +34,11 @@ function Navbar({ className }: { className?: string }) {
               <HoveredLink href="/branding">Branding</HoveredLink>
             </div>
           </MenuItem>
-          <MenuItem setActive={setActive} active={active} item="Products">
-            {/* <div className="  text-sm grid grid-cols-2 gap-10 p-4">
-            <ProductItem
-              title="Algochurn"
-              href="https://algochurn.com"
-              src="https://assets.aceternity.com/demos/algochurn.webp"
-              description="Prepare for tech interviews like never before."
-            />
-            <ProductItem
-              title="Tailwind Master Kit"
-              href="https://tailwindmasterkit.com"
-              src="https://assets.aceternity.com/demos/tailwindmasterkit.webp"
-              description="Production ready Tailwind css components for your next project"
-            />
-            <ProductItem
-              title="Moonbeam"
-              href="https://gomoonbeam.com"
-              src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.51.31%E2%80%AFPM.png"
-              description="Never write from scratch again. Go from idea to blog in minutes."
-            />
-            <ProductItem
-              title="Rogue"
-              href="https://userogue.com"
-              src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.47.07%E2%80%AFPM.png"
-              description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
-            />
-          </div> */}
-          </MenuItem>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Products"
+          ></MenuItem>
           <MenuItem setActive={setActive} active={active} item="Pricing">
             <div className="flex flex-col space-y-4 text-sm">
               <HoveredLink href="/hobby">Hobby</HoveredLink>
