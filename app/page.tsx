e628368ff7dced8562c21fdb53edacf8b0779170
Jovanney <100790418+Jovanney@users.noreply.github.com>
2024-05-23 16:27:05 -0300

    feat: adding FlipWords component and galery route

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
 import { BackgroundBeams } from "@/components/ui/background-beams";
+import { FlipWords } from "@/components/ui/flip-words";
 
 export default function Home() {
   return (
     <div className="h-screen w-full bg-slate-800 dark:bg-secondary relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-2xl mx-auto p-4">
-        <h1 className="relative z-10 text-xl md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-gray-500  lg:from-white lg:to-gray-700 dark:from-neutral-200 dark:to-neutral-600  text-center font-sans font-bold">
-          Welcome to the CinLogos AI
-        </h1>
+        <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+          Welcome to the CinLogos AI <br /> Get{" "}
+          <FlipWords
+            words={[
+              "Innovatives",
+              "Inspirings",
+              "Elegants",
+              "Moderns",
+              "Professionals",
+            ]}
+          />{" "}
+          Logos
+        </div>
+
         <p className="dark:text-neutral-400 text-gray-300 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
           CinLogos AI is a platform that allows you to create logos for your
           company or brand in a simple and fast way. Just enter the name of your
