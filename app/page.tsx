5b5197ba0169b7e57a65933bdf81d308a8a36d55
Jovanney <100790418+Jovanney@users.noreply.github.com>
2024-07-01 20:12:43 -0300

    feat: Add react-icon-cloud package for cloud icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,29 @@
-import { BackgroundBeams } from "@/components/ui/background-beams";
-import { FlipWords } from "@/components/ui/flip-words";
+import { LogoIconsCloud } from "@/components/logos-icon";
+import { Button } from "@/components/ui/button";
+import LinearGradient from "@/components/ui/linear-gradient";
 
 export default function Home() {
   return (
-    <div className="h-screen w-full bg-slate-800 dark:bg-secondary relative flex flex-col items-center justify-center antialiased">
-      <div className="max-w-2xl mx-auto p-4">
-        <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
-          Welcome to the CinLogos AI <br /> Get{" "}
-          <FlipWords words={["Perfects", "Moderns", "Professionals"]} /> Logos
-        </div>
-
-        <p className="dark:text-neutral-400 text-gray-300 max-w-lg mx-auto my-2 text-sm text-center relative z-10">
-          CinLogos AI is a platform that allows you to create logos for your
-          company or brand in a simple and fast way. Just enter the name of your
-          company or brand and we will generate a logo for you.
+    <div className="h-screen w-full px-8 relative flex flex-col items-center justify-center antialiased">
+      <div className="max-w-4xl mx-auto p-4 text-center">
+        <h1 className="md:text-5xl text-2xl font-bold text-black dark:text-neutral-200 mb-4">
+          Create Your Brand's Identity with AI
+        </h1>
+        <LogoIconsCloud />
+        <p className="text-black dark:text-neutral-200 text-lg mx-auto my-4 max-w-3xl">
+          Welcome to the future of branding. Generate unique logos and bespoke
+          soundtracks for your business in minutes with the power of AI.
         </p>
+        <div className="flex flex-col sm:flex-row justify-center gap-4 mt-8">
+          <Button className=" text-blue-800 bg-gray-100 border border-black hover:bg-blue-100 dark:bg-neutral-800 dark:text-neutral-200 dark:hover:bg-neutral-700 px-6 py-3 rounded-md font-medium">
+            Generate Logo
+          </Button>
+          <Button className=" text-blue-800 bg-gray-100 border border-black hover:bg-blue-100 dark:bg-neutral-800 dark:text-neutral-200 dark:hover:bg-neutral-700 px-6 py-3 rounded-md font-medium">
+            Create Soundtrack
+          </Button>
+        </div>
       </div>
-      <BackgroundBeams />
+      <LinearGradient />
     </div>
   );
 }
