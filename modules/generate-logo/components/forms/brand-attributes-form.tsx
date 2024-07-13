4b1c60584215ffcafa82b76b29f9429929fd9711
Jovanney <100790418+Jovanney@users.noreply.github.com>
2024-07-13 15:13:34 -0300

    feat: Update brand attributes form to use MultipleSelectWithImages component

--- modules/generate-logo/components/forms/brand-attributes-form.tsx
+++ modules/generate-logo/components/forms/brand-attributes-form.tsx
@@ -1,104 +1,108 @@
 import AutoForm from "@/components/ui/auto-form";
-import {
-  AutoFormInputComponentProps,
-  DependencyType,
-} from "@/components/ui/auto-form/types";
+import { AutoFormInputComponentProps } from "@/components/ui/auto-form/types";
 import {
   FormControl,
   FormDescription,
   FormItem,
   FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import React from "react";
-import { object, z } from "zod";
-import SelectWithImages from "../select-with-image";
+import { z } from "zod";
+import MultipleSelectWithImages from "../multiple-select-with-image";
+import { useCompanyStore } from "../../stores";
 
 interface CompanyFormProps {
   setContinue: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const BrandAttributeSchema = z.object({
-  brandAttributes: z.array(z.enum(["oi"])).nonempty(),
+  brandAttributes: z.array(z.string()),
 });
 
 export function BrandAttributeForm({ setContinue }: CompanyFormProps) {
+  const { brandAttributes, setBrandAttributes } = useCompanyStore((state) => {
+    return {
+      brandAttributes: state.Attributes,
+      setBrandAttributes: state.setAttributes,
+    };
+  });
+
   return (
     <AutoForm
+      values={{ brandAttributes: brandAttributes }}
       fieldConfig={{
         brandAttributes: {
-          // fieldType: ({
-          //   field,
-          //   label,
-          //   isRequired,
-          //   fieldConfigItem,
-          //   fieldProps,
-          // }: AutoFormInputComponentProps) => {
-          //   console.log("field.onChange:", field.onChange);
-          //   return (
-          //     <FormItem className="flex gap-2 flex-col items-center space-x-3 space-y-0 rounded-md p-4 w-full">
-          //       <FormLabel>
-          //         <span className="text-2xl text-center">{label}</span>
-          //         {isRequired && (
-          //           <span className="text-destructive dark:text-red-500">
-          //             {" "}
-          //             *
-          //           </span>
-          //         )}
-          //       </FormLabel>
-          //       <FormControl>
-          //         <SelectWithImages
-          //           options={[
-          //             {
-          //               value: "Consistency",
-          //               image: "https:via.placeholder.com/150",
-          //             },
-          //             {
-          //               value: "Innovation",
-          //               image: "https:via.placeholder.com/150",
-          //             },
-          //             {
-          //               value: "Passion",
-          //               image: "https:via.placeholder.com/150",
-          //             },
-          //             {
-          //               value: "Credibility",
-          //               image: "https:via.placeholder.com/150",
-          //             },
-          //             {
-          //               value: "Uniqueness",
-          //               image: "https:via.placeholder.com/150",
-          //             },
-          //             {
-          //               value: "Sustanability",
-          //               image: "https:via.placeholder.com/150",
-          //             },
-          //             {
-          //               value: "Authenticity",
-          //               image: "https:via.placeholder.com/150",
-          //             },
-          //             {
-          //               value: "Brand",
-          //               image: "https:via.placeholder.com/150",
-          //             },
-          //             {
-          //               value: "Accountability",
-          //               image: "https:via.placeholder.com/150",
-          //             },
-          //           ]}
-          //           value={field.value}
-          //           onChange={field.onChange}
-          //         />
-          //       </FormControl>
-          //       <div className="space-y-1 leading-none">
-          //         {fieldConfigItem.description && (
-          //           <FormDescription>
-          //             {fieldConfigItem.description}
-          //           </FormDescription>
-          //         )}
-          //       </div>
-          //     </FormItem>
-          //   );
-          // },
+          fieldType: ({
+            field,
+            label,
+            isRequired,
+            fieldConfigItem,
+          }: AutoFormInputComponentProps) => (
+            <FormItem
+              id="brand-attributes"
+              className="flex gap-2 flex-col items-center space-x-3 space-y-0 rounded-md p-4 w-full"
+            >
+              <FormLabel htmlFor="brand-attributes">
+                <span className="text-2xl text-center">{label}</span>
+                {isRequired && (
+                  <span className="text-destructive dark:text-red-500"> *</span>
+                )}
+              </FormLabel>
+              <FormControl>
+                <MultipleSelectWithImages
+                  options={[
+                    {
+                      value: "Restaurant",
+                      image: "https://via.placeholder.com/150",
+                    },
+                    {
+                      value: "Consulting",
+                      image: "https://via.placeholder.com/150",
+                    },
+                    {
+                      value: "Beauty",
+                      image: "https://via.placeholder.com/150",
+                    },
+                    {
+                      value: "Photography",
+                      image: "https://via.placeholder.com/150",
+                    },
+                    {
+                      value: "Mock",
+                      image: "https://via.placeholder.com/150",
+                    },
+                    {
+                      value: "Mock 2",
+                      image: "https://via.placeholder.com/150",
+                    },
+                    {
+                      value: "Mock 3",
+                      image: "https://via.placeholder.com/150",
+                    },
+                    {
+                      value: "Mock 4",
+                      image: "https://via.placeholder.com/150",
+                    },
+                    {
+                      value: "Mock 5",
+                      image: "https://via.placeholder.com/150",
+                    },
+                  ]}
+                  values={field.value}
+                  onChange={setBrandAttributes}
+                />
+              </FormControl>
+              <div className="space-y-1 leading-none">
+                {fieldConfigItem.description && (
+                  <FormDescription>
+                    {fieldConfigItem.description}
+                  </FormDescription>
+                )}
+              </div>
+              <FormMessage />
+            </FormItem>
+          ),
         },
       }}
       onSubmit={(value) => {
