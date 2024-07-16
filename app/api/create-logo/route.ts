42a899789b696593fd16e7ccebecdf667ec9858c
Jovanney <100790418+Jovanney@users.noreply.github.com>
2024-07-16 15:30:49 -0300

    feat: passing the user prompt to openAi to improve the prompt before the request is sent to the sunoAI

--- app/api/create-logo/route.ts
+++ app/api/create-logo/route.ts
@@ -1,11 +1,13 @@
 import { createLogo } from "@/modules/generate-logo/http/create-logo";
 import { NextRequest, NextResponse } from "next/server";
-
+import OpenAI from "openai";
 interface RequestData {
   brandAttributes: string[];
   companyName: string;
   companySegment: string;
 }
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 export async function POST(request: NextRequest) {
   try {
@@ -33,7 +35,31 @@ export async function POST(request: NextRequest) {
       ", "
     )} company named ${companyName} in the ${companySegment} segment.`;
 
-    const sunoAiResponse = await createLogo({ prompt });
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content: `Improve this prompt it will be used to generate a logo with AI, the prompt: ${prompt}`,
+        },
+      ],
+      model: "gpt-4o-2024-05-13",
+    });
+
+    const resultPrompt = completion.choices[0].message.content;
+
+    if (!resultPrompt) {
+      return new NextResponse(
+        JSON.stringify({ error: "Internal Server Error" }),
+        {
+          status: 500,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    const sunoAiResponse = await createLogo({ prompt: resultPrompt });
 
     return NextResponse.json(sunoAiResponse);
   } catch (error) {
